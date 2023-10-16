b75f3f4b854c9bf77ac9761d8482741810136839
Felipe Badia <137180694+felipebdia@users.noreply.github.com>
2023-10-16 11:29:50 -0300

    att2

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-function contar(){
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo  = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        res.innerHTML = 'Impossível contar'
-        //alert('[ERRO] Faltam dados!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0 ) {
-            alert('Passo inválido! Considerando PASSO 1')
-            p = 1
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${} x ${} = ${}`
         }
-        if (i < f) {
-            // Contagem crescente
-             for (let c = i; c <= f; c += p){
-                res.innerHTML += `${c}  \u{1F449}	`
-            }
-        } else {
-            // Contagem regressiva
-                for (let c = i; c >= f; c -= p) {
-                    res.innerHTML += `${c}  \u{1F449}	`
-                }
-            } 
-            res.innerHTML += `\u{1F3C1}`
     }
+    
 }
